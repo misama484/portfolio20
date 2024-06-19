0d99538e300f5539670a0f002b023239c8cb0921
Miguel Sanchez Martínez <msanchez@inired.com>
2024-06-19 09:38:34 +0200

    Finalizada WorkPage

--- app/Work/page.jsx
+++ app/Work/page.jsx
@@ -1,7 +1,13 @@
 "use client";
 import { useState } from "react";
 import { motion } from 'framer-motion';
-
+import Link from "next/link";
+import {TooltipProvider, Tooltip, TooltipTrigger, TooltipContent} from "../../components/ui/tooltip";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import Image from "next/image";
+import WorkSliderBtns from "../../components/ui/WorkSliderBtns"
 
 const projects = [
   {
@@ -171,7 +177,7 @@ const projects = [
     ],
     category: "Web",
     github: "https://github.com/misama484/PortFolioReact3",
-    webapp: "",
+    webapp: "https://miguelsanchez.dev",
     member: [
        {
          name: "Miguel Sanchez",
@@ -262,25 +268,104 @@ const projects = [
 
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    //get current slide index
+    const currentIndex = swiper.activeIndex;
+    // update project state based on current slide index
+    setProject(projects[currentIndex]);
+
+
+  }
+
   return (
     <motion.section
-      initial = {{ opaciti: 0 }}
-      animate = {{ opaciti:1 }}
+      initial = {{ opacity: 0 }}
+      animate = {{ opacity: 1, transition:{ delay:1.4, duration: 0.4, ease:"easeIn"} }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div>
+            <div className="flex flex-col gap-[30px] h-[50%]">
               {/* outline num */}
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.id}</div>
+              {/* project name */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.title}</h2>
               {/* project category */}
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category}</h2>
+              <p className="text-white/60 capitalize">{project.category}</p>
               {/* project Description */}
-              <p>{project.description}</p>
+              <p className="text-white/60">{project.description}</p>
+              {/* technologies */}
+              <ul className="flex gap-4">
+                {project.tags.map((tag, index) => (                  
+                  <li key={index} className="text-xl text-accent">
+                    {tag}
+                    {/* quitar ultima coma */}
+                    {index !== project.tags.length -1 && ","}
+                  </li>
+                ))}
+              </ul>
+              {/* Borde */}
+              <div className="border border-white/20"></div>
+              {/* Botones */}
+              <div className="flex items-center gap-4">
+                {/* Boton Ver Proyecto */}
+                <Link href={project.webapp}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Ver proyecto</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                
+                {/* Boton Github */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent"/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Repositorio Github</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              
+              </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%] ">slider</div>
+          <div className="w-full xl:w-[50%] ">
+            <Swiper 
+            spaceBetween={30} 
+            slidesPerView={1} 
+            className="xl:h-[520px] mb-12"
+            onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    {/* overlay */}
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                    {/* image */}
+                    <div className="relative w-full h-full">
+                      <Image src={project.image} fill className="object-cover" alt=""/>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+              })}
+              {/* slider buttons */}
+              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"/>
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
