78c20e6d5be91f80370bf1e71921b1c6bc63a629
misama.dev <misama.dev@gmail.com>
2024-06-19 11:24:32 +0000

    finalizado contactPage desde idx

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -40,12 +40,55 @@ const Contact = () => {
           <div className="xl:h-[54%] ordere-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"> 
               <h3 className="text-4xl text-accent">Contactame!</h3>
-              <p className="text-white/60">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique deleniti, beatae architecto voluptatibus distinctio error explicabo itaque nostrum accusamus ipsam eos nobis illo qui minima fugit rerum perspiciatis earum. Consequuntur?</p>
-            
+              <p className="text-white/60">Lorem ipsum dolor, sit amet consectetur 
+              adipisicing elit. Similique deleniti, beatae architecto voluptatibus 
+              distinctio error explicabo itaque nostrum accusamus ipsam eos nobis 
+              illo qui minima fugit rerum perspiciatis earum. Consequuntur?</p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+                <Input type="firstname" placeholder="Nombre" />
+                <Input type="lastname" placeholder="Apellido"/>
+                <Input type="email" placeholder="Email"/>
+                <Input type="phone" placeholder="Telefono"/>
+                
+              </div>
+              {/* Select service */}
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder= "Selecciona un servicio"/>                  
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Selecciona un servicio</SelectLabel>
+                    <SelectItem value="1">Desarrollo Web</SelectItem>
+                    <SelectItem value="2">Tienda online</SelectItem>
+                    <SelectItem value="3">App Movil</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* TextArea */}
+              <Textarea placeholder="Mensaje"/>
+              <Button size="md" className="max-w-40">Enviar</Button>
             </form>
+            
           </div>
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xk:mb-0">info</div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) =>{
+                return(
+                  <li ket={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>                                       
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>                     
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </motion.section>
