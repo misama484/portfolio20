c3b927491f0e9d14bb51b74de5a3f17befe679fd
Miguel <misama484@gmail.com>
2024-07-03 10:56:11 +0200

    cambios, revisar porque hace peticiones a firebase indiscrminadas

--- app/Work/pageBackup.jsx
+++ app/Work/pageBackup.jsx
@@ -294,7 +294,7 @@ const Work = () => {
         arrayWorks.push({...doc.data()});
     });
     setProjectsDb(arrayWorks);
-    //console.log(arrayWorks);
+    console.log(arrayWorks);
   }
   fetchWorks();  
   },);
@@ -306,26 +306,24 @@ const Work = () => {
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
-        <div className="grid grid-cols-2 xl:flex-row xl:gap-[30px]">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            {projectsDb.map((project, index) => {
-              return(
-                <div key={index} className="flex flex-col gap-[30px] h-[50%]">
+          <div key={index} className="flex flex-col gap-[30px] h-[50%]">
                 {/* outline num */}
                 <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.id}</div>
                 {/* project name */}
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.nombre}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.title}</h2>
                 {/* project category */}
                 <p className="text-white/60 capitalize">{project.category}</p>
                 {/* project Description */}
-                <p className="text-white/60">{project.descripcion}</p>
+                <p className="text-white/60">{project.description}</p>
                 {/* technologies */}
                 <ul className="flex gap-4">
-                  {project.tecnologias.map((tag, index) => (                  
+                  {project.tags.map((tag, index) => (                  
                     <li key={index} className="text-xl text-accent">
                       {tag}
                       {/* quitar ultima coma */}
-                      {index !== project.tecnologias.length -1 && ","}
+                      {index !== project.tags.length -1 && ","}
                     </li>
                   ))}
                 </ul>
@@ -335,7 +333,7 @@ const Work = () => {
                 {/* Botones */}
                 <div className="flex items-center gap-4">
                   {/* Boton Ver Proyecto */}
-                  <Link href={""}>
+                  <Link href={project.webapp}>
                     <TooltipProvider delayDuration={100}>
                       <Tooltip>
                         <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -349,7 +347,7 @@ const Work = () => {
                   </Link>
                   
                   {/* Boton Github */}
-                  <Link href={""}>
+                  <Link href={project.github}>
                     <TooltipProvider delayDuration={100}>
                       <Tooltip>
                         <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -363,10 +361,32 @@ const Work = () => {
                   </Link>              
                 </div>
               </div>
-              )
-            })}
           </div>
-          
+          <div className="w-full xl:w-[50%] ">
+            <Swiper 
+            spaceBetween={30} 
+            slidesPerView={1} 
+            className="xl:h-[520px] mb-12"
+            onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    {/* overlay */}
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                    {/* image */}
+                    <div className="relative w-full h-full">
+                      <Image src={project.image} fill className="object-cover" alt=""/>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+              })}
+              {/* slider buttons */}
+              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"/>
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
@@ -374,11 +394,3 @@ const Work = () => {
 }
 
 export default Work
-
-
-{/* --CODIGO GUARDADO--
-  
-  
-  
-  
-*/}
